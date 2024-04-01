5bfd359d4c8435079b41e14dd8152beb60cd1500
turistas <leticiapires251106@gmail.com>
2024-04-01 11:48:53 -0300

    fazendo a barrinha

--- turistas/MainPage.xaml.cs
+++ turistas/MainPage.xaml.cs
@@ -14,5 +14,6 @@ public partial class MainPage : ContentPage
 	  
 	}
 
+
 }
 
--- turistas/PaginaUmPage.xaml.cs
+++ turistas/PaginaUmPage.xaml.cs
@@ -11,7 +11,22 @@ public partial class PaginaUmPage : ContentPage
 
 	}
 
+ private void Clicarvoltar(object sender, EventArgs args) 
+	{
+	  Application.Current.MainPage = new MainPage();
+	  
+	}
 
+
+       {atual= fazenda;
+
+       imgPersonagem.Source = atual.GetNomeDaImagem(); 
+       progressBarFome.Progress = atual.GetFome();
+	   }
+void quandoclicartrocar(object sender, EventArgs args) 
+{
+	atual = nome da personagem;
+}
 
 
 
